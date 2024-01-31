dcc53c58334308628e47b42ca143cd2fb99da3d4
정승현 <tmdgusya@naver.com>
2024-02-01 01:38:38 +0900

    learning asynchrous programming

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,5 +1,22 @@
 import 'package:dart_playground/dart_playground.dart' as dart_playground;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_playground.calculate()}!');
+  Future<String> name = Future.value('Roach');
+  Future<int> age = Future.value(30);
+  Future<bool> isAdult = Future.value(true);
+
+  print('start program');
+
+  addNumbers(1, 2);
+  addNumbers(2, 3);
+}
+
+void addNumbers(int number1, int number2) async {
+  print('addNumbers: ${number1 + number2}');
+
+  await Future.delayed(Duration(seconds: 3), () {
+    print('finish calculation, answer: ${number1 + number2}');
+  });
+
+  print('end addNumbers');
 }
